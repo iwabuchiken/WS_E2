a8b847b1e2ce6c66787119d259f68448d7f96117
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-01 09:44:50 +0900

    D-11-1
    s-1#2
    ---
    1. use "PORTBbits.X = YY"
    
    	=> n/w

--- D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/D-11-1_PIC16F84A_retry.c
+++ D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/D-11-1_PIC16F84A_retry.c
@@ -47,9 +47,25 @@ void main(void) {
 	// setups
 
 	///////////////////////
-	TRISB = 0x00;	// RB3:output
+	/****************
+	 * OPTION_REG
+	 ****************/
+	OPTION_REGbits.nRBPU = 0;
 
+	/****************
+	 * TRIS
+	 ****************/
+	TRISB = 0b00000000;		// all output
+	TRISA = 0x00000000;		// RA => all output
+
+	/****************
+	 * PORT
+	 ****************/
+	PORTA = 0b00000000;		// initial => no output
 	PORTB = 0x00;
+//	TRISB = 0x00;	// RB3:output
+//
+//	PORTB = 0x00;
 
 	///////////////////////////////
 	//
@@ -64,12 +80,18 @@ void main(void) {
 	 ///////////////////////////////
 	while(1) {
 
-		PORTB = 0x01;
+		PORTBbits.RB0 = 0;
+		PORTBbits.RB1 = 1;
+
+//		PORTB = 0x01;
 
 		__delay_ms(100);
 
-		PORTB = 0x02;
+//		PORTB = 0x02;
 //		PORTB = 0x10;
+
+		PORTBbits.RB0 = 1;
+		PORTBbits.RB1 = 0;
 
 		__delay_ms(100);
 
