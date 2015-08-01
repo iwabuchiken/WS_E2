b6a7c4188f5e7b7ec78e3af9a21bbae1a01fa2f2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-01 10:13:27 +0900

    D-11-1
    s-1#3_p1-t1
    ---
    1. change device selection in MPLAB IDE
    
    	=> w

--- D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/D-11-1_PIC16F84A_retry.c
+++ D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/D-11-1_PIC16F84A_retry.c
@@ -10,17 +10,41 @@
 #include <xc.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
-#include "pic.h"
+//#include "pic.h"
 
+//#define _XTAL_FREQ 20000000
+#ifndef _XTAL_FREQ
 #define _XTAL_FREQ 20000000
+#endif
 
-#pragma config FOSC = HS
-#pragma config WDTE = OFF
-#pragma config PWRTE = ON
-#pragma config CP = OFF
+
+// CONFIG1
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+//#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+
+#pragma config PWRTE = ON, MCLRE = ON       // Power-up Timer Enable bit (PWRT enabled)
+//#pragma config PWRTE = ON, MCLRE = ON       // Power-up Timer Enable bit (PWRT enabled)
+//#pragma config MCLR = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+//#pragma config EXTMCLR = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+//#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+//#pragma config BODEN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+//#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config DEBUG = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+//#pragma config FOSC = HS
+//#pragma config WDTE = OFF
+//#pragma config PWRTE = ON
+//#pragma config CP = OFF
 
 
 ///////////////////////
@@ -85,7 +109,7 @@ void main(void) {
 
 //		PORTB = 0x01;
 
-		__delay_ms(100);
+		__delay_ms(500);
 
 //		PORTB = 0x02;
 //		PORTB = 0x10;
--- D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/build/default/production/D-11-1_PIC16F84A_retry.p1.d
+++ D-11-1_PIC16F84A/D-11-1_PIC16F84A_retry.X/build/default/production/D-11-1_PIC16F84A_retry.p1.d
@@ -1,4 +1,3 @@
  build/default/production/D-11-1_PIC16F84A_retry.d  \
  build/default/production/D-11-1_PIC16F84A_retry.p1:  \
- D-11-1_PIC16F84A_retry.c  \
-pic.h 
+ D-11-1_PIC16F84A_retry.c 
