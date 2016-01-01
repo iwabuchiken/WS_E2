18689e043786f22b692d3120c179259c0cd831f1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-01-01 11:56:02 +0900

    D-21
    s-4#1
    ---
    1. new project: Design01
    2. pin 1[5],1[4] => on/off
    
        ==> done

---
+++ psoc/Design01/Design01.cydsn/codegentemp/Design01.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- Design01.ctl generated from Design01
+-- 01/01/2016 at 11:46
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- M0S8 Clock Editor
+-- Directives Editor
+-- Analog Device Editor
---
+++ psoc/Design01/Design01.cydsn/main.c
@@ -0,0 +1,36 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        OUT_1_Write( 1 );
+        OUT_2_Write( 1 );
+        
+        CyDelay( 500 );
+        
+        OUT_1_Write( 0 );
+        OUT_2_Write( 0 );
+        
+        CyDelay( 500 );
+        
+    }
+}
+
+/* [] END OF FILE */
