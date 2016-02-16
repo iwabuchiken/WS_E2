629a19bfa15470f35532bb57bc2174bef40d9ff0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-02-16 09:25:33 +0900

    D-23
    s-3#1/steps.2
    ---
    1. use LCD
    2. PROB: no chars shown
    
        ==> going

---
+++ D-23_start-on-mac/D-23_s-3-1/D-23_s-3-1.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+
+//ref http://happy-arduino.blogspot.jp/2012/02/lcd_22.html
+LiquidCrystal lcd(7,8,9,10,11,12,13);
+int LCD_BL=6;
+
+void setup()
+{
+  pinMode(LCD_BL,OUTPUT);
+  lcd.begin(16, 2);//16x2桁
+}
+void loop()
+{
+  digitalWrite(LCD_BL,HIGH);
+  delay(100);
+  lcd.clear();
+ 
+  lcd.print("LCD TestPrint");
+  lcd.setCursor(0,1);
+  lcd.print("2nd TestPrint");
+}
