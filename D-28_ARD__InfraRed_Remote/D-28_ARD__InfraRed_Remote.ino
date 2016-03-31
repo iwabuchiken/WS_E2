3363aa2bfd60d057f93101acd5d3034ac0c82c1b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-31 15:28:17 +0900

    D-28
    s-2#2/steps.4
    ---
    1. TRY: "%04d %c%c %s"
    
        ==> displayed

--- D-28_ARD__InfraRed_Remote/D-28_ARD__InfraRed_Remote.ino
+++ D-28_ARD__InfraRed_Remote/D-28_ARD__InfraRed_Remote.ino
@@ -34,7 +34,7 @@
 #define IRPIN       2
 //#define DATA_POINT  5
 
-char* id = "28 2#2 s.3";
+char* id = "28 2#2 s.4";
 char* msg = "Welcome";
 
 LiquidCrystal lcd(7,8,9,10,11,12,13);
@@ -417,7 +417,11 @@ void loop() {
 //      dtostrf(temp, 3, 2, str_temp);  // n.w
       
 //      sprintf(line_2, "%04d secs %s", total, str_temp);
-      sprintf(line_2, "%04d %s %s", total, remocon_values_str[4], str_temp);
+      sprintf(line_2, "%04d %c%c %s", 
+          total, 
+          remocon_values_str[4], 
+          remocon_values_str[5], 
+          str_temp);
 
       
       //      sprintf(line_2, "%04d secs %03.2d", total, temp);
@@ -477,6 +481,7 @@ void loop() {
   
     
 }//loop()
+
 
 
 
