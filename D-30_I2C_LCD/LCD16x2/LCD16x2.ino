d213938a725561dde96651b02e170fa478d5d723
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-06-11 16:33:34 +0900

    D-30
    s-5#1/steps.~4
    ---
    1. add => PageClear() after "Arduino"
    
        ==> done
    	>RESULT: "♥rduino" ==> not shown

--- D-30_I2C_LCD/LCD16x2/LCD16x2.ino
+++ D-30_I2C_LCD/LCD16x2/LCD16x2.ino
@@ -19,6 +19,12 @@ skI2CLCDlib LCD(0x3E,16) ;              // ＬＣＤのアドレス、画面カ�
 //contrast
 int contrast = 40;
 
+//interval
+int interval = 2000;
+
+//         0111111001111110       
+char* msg_1 = "30 5#1/4        ";
+
 void setup()
 {
      // ＬＣＤモジュールの初期化処理
@@ -28,27 +34,69 @@ void setup()
 
      LCD.CreateChar(1,heart) ;          // 1番にオリジナルキャラクタを登録する
 
-     // ０ページ目、１ページ目に書き込む(０ページのみ表示される)
-     LCD.SetCursor(0,0) ;               // 表示位置を１行目１列[00H]に設定
-     LCD.Puts("Hello") ;                // [00H]から書込まれる(表示する)
-     LCD.SetCursor(16,0) ;              // 表示位置を１行目１６列[10H]に設定
-     LCD.Putc(0x01) ;                   // 1番に登録したキャラを書込む(表示しない)
-     LCD.SetCursor(0,1) ;               // 表示位置を２行目１列[40H]に設定
-     LCD.Puts("0111111001111110") ;     // [40H]から書込まれる(表示する)
-//     LCD.Puts("abcdefghijklmnop") ;     // [50H]から書込まれる(表示しない)
-     LCD.Puts("a c e ghijklmnop") ;     // [50H]から書込まれる(表示しない)
-     delay(5000) ;
-     // １ページ目に切り替えて表示する
-     LCD.PageSet(1) ;                   // 1ページ目の表示切り替え
-     delay(5000) ;
-     // １ページ目を書き換える
-     LCD.PageClear() ;                  // 1ページ目のクリア
-     LCD.PageSetCursor(0,0) ;           // 1ページ目の位置でカーソル指示、[10H]に設定
-     LCD.Puts("Arduino") ;              // 1ページ目[10H]から書込まれる(表示する)
-     delay(5000) ;
-     // ０ページ目に表示を切り替える
-     LCD.PageSet(0) ;                   // 0ページ目の表示切り替え
+//     // ０ページ目、１ページ目に書き込む(０ページのみ表示される)
+//     LCD.SetCursor(0,0) ;               // 表示位置を１行目１列[00H]に設定
+//     LCD.Puts("Hello") ;                // [00H]から書込まれる(表示する)
+//     LCD.SetCursor(16,0) ;              // 表示位置を１行目１６列[10H]に設定
+//     LCD.Putc(0x01) ;                   // 1番に登録したキャラを書込む(表示しない)
+//     LCD.SetCursor(0,1) ;               // 表示位置を２行目１列[40H]に設定
+////     LCD.Puts("0111111001111110") ;     // [40H]から書込まれる(表示する)
+//     LCD.Puts(msg_1) ;     // [40H]から書込まれる(表示する)
+//     
+////     LCD.Puts("abcdefghijklmnop") ;     // [50H]から書込まれる(表示しない)
+//     LCD.Puts("a c e ghijklmnop") ;     // [50H]から書込まれる(表示しない)
+//     delay(5000) ;
+//     // １ページ目に切り替えて表示する
+//     LCD.PageSet(1) ;                   // 1ページ目の表示切り替え
+//     delay(5000) ;
+//     // １ページ目を書き換える
+//     LCD.PageClear() ;                  // 1ページ目のクリア
+//     LCD.PageSetCursor(0,0) ;           // 1ページ目の位置でカーソル指示、[10H]に設定
+//     LCD.Puts("Arduino") ;              // 1ページ目[10H]から書込まれる(表示する)
+//     delay(5000) ;
+//     // ０ページ目に表示を切り替える
+//     LCD.PageSet(0) ;                   // 0ページ目の表示切り替え
+     
 }
+
 void loop()
 {
+  
+  exec();
+  
 }
+
+void exec() {
+  
+    // ０ページ目、１ページ目に書き込む(０ページのみ表示される)
+    LCD.SetCursor(0,0) ;               // 表示位置を１行目１列[00H]に設定
+    LCD.Puts("Hello") ;                // [00H]から書込まれる(表示する)
+    LCD.SetCursor(16,0) ;              // 表示位置を１行目１６列[10H]に設定
+    LCD.Putc(0x01) ;                   // 1番に登録したキャラを書込む(表示しない)
+    LCD.SetCursor(0,1) ;               // 表示位置を２行目１列[40H]に設定
+//    LCD.Puts("0111111001111110") ;     // [40H]から書込まれる(表示する)
+    LCD.Puts(msg_1) ;     // [40H]から書込まれる(表示する)
+    
+//    LCD.Puts("abcdefghijklmnop") ;     // [50H]から書込まれる(表示しない)
+    LCD.Puts("a c e ghijklmnop") ;     // [50H]から書込まれる(表示しない)
+    delay(interval) ;
+//    delay(5000) ;
+    // １ページ目に切り替えて表示する
+    LCD.PageSet(1) ;                   // 1ページ目の表示切り替え
+    delay(interval) ;
+//    delay(5000) ;
+    // １ページ目を書き換える
+    LCD.PageClear() ;                  // 1ページ目のクリア
+    LCD.PageSetCursor(0,0) ;           // 1ページ目の位置でカーソル指示、[10H]に設定
+    LCD.Puts("Arduino") ;              // 1ページ目[10H]から書込まれる(表示する)
+    delay(interval) ;
+//    delay(5000) ;
+    
+    LCD.PageClear() ;                  // 1ページ目のクリア
+    
+    // ０ページ目に表示を切り替える
+    LCD.PageSet(0) ;                   // 0ページ目の表示切り替え
+
+}//void exec()
+
+
