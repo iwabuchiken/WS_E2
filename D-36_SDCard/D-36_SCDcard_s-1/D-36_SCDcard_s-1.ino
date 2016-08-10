703a227a94ef5865aa826e82c11b61e3fd02c156
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-08-10 17:04:06 +0900

    D-36
    s-2#1/steps.1
    ---
    1. try tutorial
    	http://qiita.com/ohisama@github/items/4a73d133b7abff298e1e
    
        ==> n/w

--- D-36_SDCard/D-36_SCDcard_s-1/D-36_SCDcard_s-1.ino
+++ D-36_SDCard/D-36_SCDcard_s-1/D-36_SCDcard_s-1.ino
@@ -1,53 +1,59 @@
 /*
  * http://deviceplus.jp/hobby/entry021/
  */
+//const int chipSelect = 10;
+
 #include <SPI.h>
-
 #include <SD.h>
-
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-const int chipSelect = 10;
 
 //         0111111001111110       
 char* msg_1 = "36 1#1/1        ";
 
+
+File myFile;
 void setup()
 {
-  Serial.begin(9600);
-  while (!Serial) {
-  ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  Serial.print("\nInitializing SD card...");
-  pinMode(10, OUTPUT); // change this to 53 on a mega
-  
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card is inserted?");
-    Serial.println("* Is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
-    
-  } else {
-    
-    Serial.println("Wiring is correct and a card is present.");
-    
-  }
-  
-}//setup()
-
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        ;
+    }
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(4))
+    {
+        Serial.println("initialization failed!");
+        return;
+    }
+    Serial.println("initialization done.");
+    myFile = SD.open("test.txt", FILE_WRITE);
+    if (myFile)
+    {
+        Serial.print("Writing to test.txt...");
+        myFile.println("testing 1, 2, 3.");
+        myFile.close();
+        Serial.println("done.");
+    }
+    else
+    {
+        Serial.println("error opening test.txt");
+    }
+    myFile = SD.open("test.txt");
+    if (myFile)
+    {
+        Serial.println("test.txt:");
+        while (myFile.available())
+        {
+            Serial.write(myFile.read());
+        }
+        myFile.close();
+    }
+    else
+    {
+        Serial.println("error opening test.txt");
+    }
+}
 void loop()
 {
-  
-  exec();
-  
+
 }
-
-void exec() {  
-
-}//void exec()
-
-
 
