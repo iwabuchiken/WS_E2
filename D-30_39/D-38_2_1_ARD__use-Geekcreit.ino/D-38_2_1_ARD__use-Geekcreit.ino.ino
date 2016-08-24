4f493282b943a5ad9dfd6df833c5cc4d19f27013
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-08-24 15:17:17 +0900

    D-38
    s-2#1/steps.1
    ---
    1. display opening message
    
    	==> done

---
+++ D-30_39/D-38_2_1_ARD__use-Geekcreit.ino/D-38_2_1_ARD__use-Geekcreit.ino.ino
@@ -0,0 +1,67 @@
+/*
+ * http://deviceplus.jp/hobby/entry021/
+ */
+//const int chipSelect = 10;
+
+#include <SPI.h>
+#include <SD.h>
+
+//         0111111001111110       
+char* msg_1 = "38 2#1 steps.1";
+
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+const int chipSelect = 4;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // opening message
+  Serial.println(msg_1);
+
+  // ref http://stackoverflow.com/questions/17503094/arduinio-sd-on-ethernet-shield-not-working-at-all answered Feb 28 at 21:32
+//  SD.begin();
+  SD.begin(chipSelect);
+
+  Serial.print("\nInitializing SD card...");
+
+  // SSピン（Unoは10番、Megaは53番）は使わない場合でも出力にする必要があります。
+  // そうしないと、SPIがスレーブモードに移行し、SDライブラリが動作しなくなります。
+  pinMode(SS, OUTPUT);
+
+  // ref http://embedjournal.com/arduino-sd-card-initialization-failed/
+//  digitalWrite(10,HIGH);
+  digitalWrite(SS,HIGH);
+
+  pinMode(chipSelect,OUTPUT);
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+}
+
+void loop()
+{
+
+}
+
