6c162e8ed31ff156b8bd1c93abe2e26a4464dcda
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-08-26 18:10:45 +0900

    D-38
    s-4#1/steps.2
    ---
    1. sdcard: volume size, a.o.
    
        ==> done

--- D-30_39/D-38_4_1_ARD__use-Geekcreit.ino/D-38_4_1_ARD__use-Geekcreit.ino.ino
+++ D-30_39/D-38_4_1_ARD__use-Geekcreit.ino/D-38_4_1_ARD__use-Geekcreit.ino.ino
@@ -12,7 +12,7 @@
 #include <SPI.h>
 #include <SD.h>
 
-char* id = "38 s-4#1 steps.1";
+char* id = "38 s-4#1 steps.2";
 char* msg = "Welcome";
 
 Sd2Card card;
@@ -136,6 +136,60 @@ void _setup_SDCard() {
     Serial.println("Wiring is correct and a card is present.");
   }
 
+  /*
+   * card type
+   */
+  Serial.print("\nCard type: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+    Serial.println("SD1");
+    break;
+    case SD_CARD_TYPE_SD2:
+    Serial.println("SD2");
+    break;
+    case SD_CARD_TYPE_SDHC:
+    Serial.println("SDHC");
+    break;
+    default:
+    Serial.println("Unknown");
+  }//switch (card.type())
+
+  /*
+   * init volume
+   */
+   if (!volume.init(card)) {
+      Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+      return;
+    } else {
+
+      Serial.println("init volume => done");
+      
+    }
+
+  /*
+   * - volume size
+   * - list files
+   */
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+  volumesize = volume.blocksPerCluster(); // clusters are collections of blocks
+  volumesize *= volume.clusterCount(); // we'll have a lot of clusters
+  volumesize *= 512; // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
+  
   
 }//_setup_SDCard()
 
@@ -179,6 +233,7 @@ void conv_Count_2_ClockLabel(char* label, int count) {
 //  sprintf(label, "%d:%02d", min, sec);
   
 }
+
 
 
 
