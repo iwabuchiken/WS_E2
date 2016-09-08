31ad42c88f1b1720a65d4ce77bd55098da8d157d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-09-08 11:38:26 +0900

    D-38
    s-9#1/step.22
    ---
    1. TRY: co working commit --> 777a46785009840466af91
    
        ==> w.
    
    ===========================TEMPLATE
    1. TRY:
    
        ==>

--- D-30_39/D-38/D-38_8_2_ARD__use-Geekcreit/D-38_8_2_ARD__use-Geekcreit.ino
+++ D-30_39/D-38/D-38_8_2_ARD__use-Geekcreit/D-38_8_2_ARD__use-Geekcreit.ino
@@ -12,7 +12,7 @@
 #include <SPI.h>
 #include <SD.h>
 
-char* id = "38 s-9#1 steps.21";
+char* id = "38 s-9#1 steps.22";
 char* msg = "Welcome";
 
 Sd2Card card;
@@ -23,7 +23,7 @@ File myFile;
 
 //char* fname = "20160826.txt";
 //char* fname = "20160907.txt";
-char* fname = "16090701.txt";
+char* fname = "16090801.txt";
 //char* fname = "160907_1.txt";
 //char* fname = "160907-1.txt";
 
@@ -250,8 +250,7 @@ void _setup_SDCard() {
     
       Serial.print("Writing to: ");
       Serial.println(fname);
-
-      myFile.println("=================================");
+      
       myFile.println(id);
       myFile.close();
       
@@ -389,6 +388,7 @@ void intr_pin() {
 
 
 }
+
 
 
 
